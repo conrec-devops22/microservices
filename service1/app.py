c19ca035d56722bd875592f98cd02d9151257b15
Daniel Nyström <daniel@nystrom.st>
2023-09-13 09:50:27 +0200

    Set up structured and centralised logging

--- service1/app.py
+++ service1/app.py
@@ -1,19 +1,66 @@
-from flask import Flask
+import json
+import sys
+import logging
+from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
+
+class StructuredFormatter(logging.Formatter):
+    """
+    Structure log entries as JSON objects and add request context if available
+    """
+
+    def format(self, record):
+        log_entry = {
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "timestamp": self.formatTime(record),
+        }
+        if request:
+            log_entry.update(
+                {
+                    "path": request.path,
+                    "method": request.method,
+                    "remote_addr": request.remote_addr,
+                }
+            )
+        if record.exc_info:
+            log_entry["exception"] = self.formatException(record.exc_info)
+        return json.dumps(log_entry)
+
+
 app = Flask(__name__)
+
+# Set up our logging handler to use StructuredFormatter
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(StructuredFormatter())
+
+# Add the handler to the Flask app's logger
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
+# Also change the werkzeug logger, the default Flask logger, to use StructuredFormatter
+logging.getLogger("werkzeug").addHandler(handler)
+
+
+# Add Prometheus WSGI middleware to track metrics
 metrics = PrometheusMetrics(app)
 
-metrics.info('app_info', ' Service 1', version = '1.0.0')
+# Add service metadata for Prometheus
+metrics.info("app_info", " Service 1", version="1.0.0")
 
+
+# Add a metric to track requests by method
 @metrics.counter(
-        'requests_by_method',
-        ' Number of requests per method',
-        labels={'method': lambda r: r.method}
+    "requests_by_method",
+    " Number of requests per method",
+    labels={"method": lambda r: r.method},
 )
 @app.route("/")
-def main():
+def hello_world():
+    app.logger.info("Service 1 called")
     return "Hello, World!"
 
 
+# Important to run the app without debug mode or metrics endpoint will not be available
 app.run(host="0.0.0.0", port=5001, debug=False)
--- service2/app.py
+++ service2/app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 import requests
+import logging
+import logging.handlers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logger.info("Service 2 started")
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-metrics.info('app_info', ' Service 2', version = '1.0.0')
+metrics.info("app_info", " Service 2", version="1.0.0")
+
 
 @app.route("/")
 def main():
+    logger.info("Service 2 called")
     r = requests.get("http://service1-service:5001")
     return f"Hello from Service 2. Service 1 says: {r.text}!"
 
